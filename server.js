4b54ec9bb0c62b3fb35303547c677cb8cb4c5ca4
Rabbi <alrabbism@gmail.com>
2020-10-06 08:51:22 +0600

    port update

--- server.js
+++ server.js
@@ -1,94 +1,89 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const ObjectID = require('mongodb').ObjectID
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
-
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const port = 5000
+const app = express();
 require('dotenv').config()
-
-
-// var admin = require("firebase-admin");
-
-// var serviceAccount = require("./volunteer-network-fullstack-firebase-adminsdk-q8i4w-73316a9b54.json");
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://volunteer-network-fullstack.firebaseio.com"
-// });
-
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.fjsvr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const ObjectId = require('mongodb').ObjectId;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xjikz.mongodb.net/volunteerDB?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
 client.connect(err => {
-  const eventsCollection = client.db(process.config.DB_NAME).collection("events");
-  const registeredEvents = client.db(process.config.DB_NAME).collection("registered");
-  //mongo scope start
-  app.get('/show-volunteers', (req,res)=>{
-    eventsCollection.find({})
-    .toArray((error, documents)=>{
-      res.send(documents)
-    })
-  })
+    const volunteerCollection = client.db("volunteerDB").collection("volunteers");
 
-  app.get('/all-registered-events',(req,res)=>{
-    registeredEvents.find({})
-    .toArray((error,documents)=>{
-      res.send(documents)
-      
+    //  welcome
+    app.get("/", (req, res) => {
+        res.send("Welcome volunteer Server")
     })
-  })
+      //create volunteer
+    app.post("/addvolunteer", (req, res) => {
+        const volunteer = req.body;
+        volunteerCollection.insertOne(volunteer)
+            .then(result => {
+                console.log('One volunteer Added successfully.')
+                res.redirect('/')
+            })
+        //  console.log(req.body)
 
-  app.get('/my-events',(req,res)=>{
-    registeredEvents.find({email:req.headers.email})
-    .toArray((error,documents)=>{
-      res.send(documents)
-      
-    })
-  })
-
-    app.post('/submit-form',(req,res)=>{
-      registeredEvents.insertOne(req.body)
-      .then(result=>{
-        
-        res.send(result.insertedCount>0)
-      })
     })
 
-    app.post('/add-event',(req,res)=>{
-      eventsCollection.insertOne(req.body)
-      .then(result=>{
-        res.send(result.insertedCount>0)
-      })
+    //   read volunteer
+    app.get('/getAllVolunteer', (req, res) => {
+       
+        volunteerCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+
     })
 
+    //   read all volunteer
+    app.get('/getvolunteer', (req, res) => {
+        const queryEmail = req.query.email;
+        // console.log(queryEmail)
+        volunteerCollection.find({ email: queryEmail })
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
 
-    app.delete('/cancel-event',(req,res)=>{
-      registeredEvents.deleteOne({_id:ObjectID(req.headers.id)})
-      .then(result=>{
-        
-        res.send(result.deletedCount>0)
-      })
     })
 
-    app.delete('/delete-event',(req,res)=>{
-      registeredEvents.deleteOne({_id:ObjectID(req.headers.id)})
-      .then(result=>{
-        
-        res.send(result.deletedCount>0)
-      })
+    //   delete volunteer
+    app.delete('/deleteVlunteerList/:listId', (req, res) => {
+        //  console.log(req.params.id)
+        volunteerCollection.deleteOne({
+            _id: ObjectId(req.params.listId)
+        })
+            .then(result => {
+                res.redirect(result.deletedCount > 0)
+            })
+
     })
 
 
-  //mongo scope end
+    //   delete volunteer
+    app.delete('/delete/:id', (req, res) => {
+        //  console.log(req.params.id)
+        volunteerCollection.deleteOne({
+            _id: ObjectId(req.params.id)
+        })
+            .then(result => {
+                 res.send(result.deletedCount > 0)
+            })
+
+    })
+
+
+    console.log('database connected.')
+    //   client.close();
 });
 
 
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, ()=>{
-    console.log('Server is running with '+PORT+' port')
-})
+app.listen(process.env.PORT || port)
